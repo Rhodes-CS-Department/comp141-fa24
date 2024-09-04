d9c6932c4cec4215364f2e126dd9debf2ccebb1d
Jace O King <44843586+kingjaceo@users.noreply.github.com>
2024-09-04 11:24:03 -0500

    Update and rename daily_quizzes.md to daily_quizzes.py

--- king/daily_quizzes.md
+++
@@ -1,9 +0,0 @@
-# Trace the code. #
-
-
-### Aug 30, 2024 ###
-```
-this_can_be_called_anything = "Hello, everybody!"
-this_can_be_called_anything = "Goodbye, everybody!"
-print(this_can_be_called_anything)
-```
---
+++ king/daily_quizzes.py
@@ -0,0 +1,11 @@
+### Trace the code:
+# Trace the values of the variables and give the output of the code.
+
+# Sept 4, 2024
+first_name = 'Jace'
+last_name = 'King'
+first_name = last_name
+last_name = first_name
+print(last_name, first_name)
+
+# Sept 6, 2024
