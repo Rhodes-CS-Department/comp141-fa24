ed0f8625b33855e52040e7e50d850ca84c8e759e
Jace O King <44843586+kingjaceo@users.noreply.github.com>
2024-09-11 09:43:51 -0500

    Update daily_quizzes.py

--- king/daily_quizzes.py
+++ king/daily_quizzes.py
@@ -15,3 +15,36 @@ print("Now watch this:")
 tens = int(user_input) // 10
 ones = int(user_input) % 10
 print("There are", tens, "tens and", ones, "ones in", user_input)
+
+# Sept 9, 2024
+a = 10
+b = 12
+c = 13
+if a < b:
+  print("red")
+else:
+  print("blue")
+if (a + b) % 2 == 0:
+  print(a + b, " is even")
+if c % 2 == 0:
+  print(c, " is even")
+else:
+  print("Something is odd...")
+
+# Sept 11, 2024
+cond1 = 3 < 4
+cond2 = False
+if cond1:
+  cond2 = True
+  print("3 IS less than 4")
+elif cond2:
+  print("cond2 is True")
+else:
+  print("Nothing interesting happened.")
+
+if cond1:
+  print("3 is STILL less than 4")
+if cond2:
+  print("cond2 is True")
+else:
+  print("Still nothing interesting happened.")
