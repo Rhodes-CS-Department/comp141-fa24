f0b1747eb6534f04fb6781957357e51766afcabd
Jace O King <44843586+kingjaceo@users.noreply.github.com>
2024-10-14 11:31:48 -0500

    Update daily_quizzes_part2.py

--- king/daily_quizzes_part2.py
+++ king/daily_quizzes_part2.py
@@ -8,3 +8,18 @@ def boolean_tests(a, b, c, d):
 def main():
   boolean_tests(True, True, 10, 20)
   boolean_tests(False, True, 12, 20)
+
+# Oct 14, 2024
+def new_idea(n):
+  for i in range(n):
+    for j in range(n):
+      print(i, j)
+
+def newer_idea(n):
+  for i in range(n):
+    for j in range(i):
+      print(i, j)
+
+def main():
+  new_idea(5)
+  newer_idea(5)
