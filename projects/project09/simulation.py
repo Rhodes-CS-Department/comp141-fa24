14ece8ff4a8d2364b0564a977e0e89deaa826bb7
Jace O King <44843586+kingjaceo@users.noreply.github.com>
2024-11-29 22:18:51 -0600

    Update simulation.py

--- projects/project09/simulation.py
+++ projects/project09/simulation.py
@@ -1,3 +1,8 @@
+# Author: FILL IN
+# Class: COMP 141
+# Program 9: Predator-Prey Simulation
+# Pledge: I have neither given nor received unauthorized aid on this program. 
+# Description: FILL IN
 from cs1.graphics import *
 import random
 
